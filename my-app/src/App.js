05892884774acfb17694875c15380053dcf2dadc
lassebramer <lassebramer@gmail.com>
2018-03-20 11:25:42 +0100

    Grid layout

---
+++ my-app/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container">
+        <header> "HEADER" </header>
+        <div class="game">
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+          <div class="tile"> 1 </div>
+        </div>
+        <div class="left"> "LEFT" </div>
+        <div class="right"> "Right" </div>
+        <footer> "FOOTER BABY" </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
